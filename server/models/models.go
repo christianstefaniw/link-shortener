e2b95867bc5796936d7d124f618fd22493e478f3
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-03-09 12:08:01 -0500

    small refactor

--- server/models/models.go
+++ server/models/models.go
@@ -1,8 +1,8 @@
 package models
 
 type ShortenedURL struct {
-	FullURL  string `json:”title,omitempty”`
-	ShortURL string `json:”title,omitempty”`
+	FullURL  string
+	ShortURL string
 }
 
 type URLS struct {
